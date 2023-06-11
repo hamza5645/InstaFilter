37f02f9766d74d4415fce6287db81e7aca80a24d
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-06-11 23:26:44 +0300

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Hamza Osama on 11/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Hamza Osama on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
