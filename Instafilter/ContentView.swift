8b875b96e8052b8d2cab20a544e88d4305dc7967
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-06-12 09:25:34 +0300

    Experimenting how property wrappers work work

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount = 0.0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+                .onChange(of: blurAmount) { newValue in
+                    print("\(newValue)")
+                }
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
         }
-        .padding()
     }
 }
 
