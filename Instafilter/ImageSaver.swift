13a7e7ba03bf2aed029eebb947371d0b687dd144
hamza5645 <97033430+hamza5645@users.noreply.github.com>
2023-06-20 15:03:16 +0300

    Save image to the user library

---
+++ Instafilter/ImageSaver.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Hamza Osama on 20/06/2023.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
